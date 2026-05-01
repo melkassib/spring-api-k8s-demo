cefc9c5f56ae221deadd6054631e19bd7b290d62
Mohcine EL KASSIB <elkassib.mohcine@gmail.com>
2026-05-02 01:19:42 +0200

    bootstrap project

---
+++ src/main/java/com/melkassib/demo/SpringApiK8sDemoApplication.java
@@ -0,0 +1,13 @@
+package com.melkassib.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiK8sDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringApiK8sDemoApplication.class, args);
+    }
+
+}
